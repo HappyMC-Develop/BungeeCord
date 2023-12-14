f56ed151cb34f78dc7214acd3c5c811f163e6b83
JSWuMing <57783981+JSWuMing@users.noreply.github.com>
2023-12-15 01:40:32 +0800

    Update CaptchaGeneration.java
    对CaptchaGeneration.java使用谷歌翻译

--- proxy/src/main/java/ru/leymooo/botfilter/captcha/CaptchaGeneration.java
+++ proxy/src/main/java/ru/leymooo/botfilter/captcha/CaptchaGeneration.java
@@ -54,13 +54,13 @@ public class CaptchaGeneration
         ThreadPoolExecutor ex = (ThreadPoolExecutor) executor;
         while ( ex.getActiveCount() != 0 )
         {
-            BungeeCord.getInstance().getLogger().log( Level.INFO, "[BotFilter] Генерирую капчу [{0}/900]", 900 - ex.getQueue().size() - ex.getActiveCount() );
+            BungeeCord.getInstance().getLogger().log( Level.INFO, "[BotFilter] 验证码生成中…… [{0}/900]", 900 - ex.getQueue().size() - ex.getActiveCount() );
             try
             {
                 Thread.sleep( 1000L );
             } catch ( InterruptedException ex1 )
             {
-                BungeeCord.getInstance().getLogger().log( Level.WARNING, "[BotFilter] Не могу сгенерировать капчу. Выключаю банджу", ex1 );
+                BungeeCord.getInstance().getLogger().log( Level.WARNING, "[BotFilter] 验证码生成失败，即将关闭", ex1 );
                 System.exit( 0 );
                 return;
             }
@@ -68,7 +68,7 @@ public class CaptchaGeneration
         CachedCaptcha.generated = true;
         executor.shutdownNow();
         System.gc();
-        BungeeCord.getInstance().getLogger().log( Level.INFO, "[BotFilter] Капча сгенерированна за {0} мс", System.currentTimeMillis() - start );
+        BungeeCord.getInstance().getLogger().log( Level.INFO, "[BotFilter] 验证码生成成功耗时{0} ms", System.currentTimeMillis() - start );
     }
 
 
